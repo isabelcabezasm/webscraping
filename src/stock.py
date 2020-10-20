987d51662880d2c30716ec78f31ae79f3cd73840
Isabel Cabezas <isabelcabezasm@uoc.edu>
2020-10-20 21:23:04 +0200

    get link for company page details

--- src/stock.py
+++ src/stock.py
@@ -28,20 +28,27 @@ class StockScraper():
         array = [i.text for i in cells]        
         return array
 
+    def __getLinkURL(self, row):
+        link = row.find('a')
+        return link.get('href')
+
     def scrape(self):
         print ("Stocks Web Scraping  from " + "'" + self.url + "'...")
-        print ("Hello world")
 
         page = self.__getHtml(self.url+self.subdomain)
         soup = BeautifulSoup(page.text, 'html.parser') 
 
         stockTable = self.__getStockTable(soup, 'ctl00_Contenido_tblAcciones')
+        detailLinks = []
+        stockInfo = []
 
         for row in stockTable: 
-            cells = self.__getArrayFromRow(row)
-
-            #todo: get link to the detail page here.
-
+            cells = self.__getArrayFromRow(row)            
             if len(cells) != 0:
-                print(cells)
+                detailLinks.append(self.__getLinkURL(row))
+                stockInfo.append(cells)
 
+        print(detailLinks)
+        print(stockInfo)
+
+        #todo: add this information into a file
