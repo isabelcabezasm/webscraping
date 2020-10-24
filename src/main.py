28464f211657c3136f8cd35913babd5e791f0433
Jorge-Saura <jorsaura@gmail.com>
2020-10-24 12:39:06 +0200

    He sacado los parámetros de url y domain fuera de la clase scrapper

--- src/main.py
+++ src/main.py
@@ -2,6 +2,8 @@ from stock import StockScraper
 
 output_file = "dataset.csv"
 
+url = "https://www.bolsamadrid.es"
+domain = "/esp/aspx/Mercados/Precios.aspx?indice=ESI100000000"
 stock = StockScraper()
 stock.scrape()
 # stock.data2csv(output_file);
--- src/stock.py
+++ src/stock.py
@@ -11,9 +11,9 @@ class StockScraper():
         return "nothing"
     
     
-    def __init__(self):
-            self.url = "https://www.bolsamadrid.es"
-            self.subdomain = "/esp/aspx/Mercados/Precios.aspx?indice=ESI100000000"
+    def __init__(self, url,subdomain):
+            self.url = url #"https://www.bolsamadrid.es"
+            self.subdomain = subdomain #"/esp/aspx/Mercados/Precios.aspx?indice=ESI100000000"
             self.data = []
 
     def __getHtml(self, url):
