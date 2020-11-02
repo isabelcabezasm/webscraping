31117c23aba5682e4538c26746a5b1bf71d910eb
Isabel Cabezas <isabelcabezasm@uoc.edu>
2020-11-02 23:04:49 +0100

    limpiando lineas de código

--- src/main.py
+++ src/main.py
@@ -1,9 +1,6 @@
 from stock import StockScraper
 
-output_file = "dataset.csv"
-
 url = "https://www.bolsamadrid.es"
 domain = "/esp/aspx/Mercados/Precios.aspx?indice=ESI100000000"
 stock = StockScraper(url, domain)
 stock.scrape()
-# stock.data2csv(output_file);cls
